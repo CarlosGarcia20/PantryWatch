b137c7649ba3e70d4de922588420a6276b61cccb
CarlosGarcia20 <cargarcia085@gmail.com>
2025-12-13 21:58:56 -0700

    Se actualizó la manera en como se regresan los datos al ESP32

--- src/controllers/sensor.controller.js
+++ src/controllers/sensor.controller.js
@@ -1,41 +1,34 @@
 import { prologService } from "../services/prologService.js";
 
 export class sensorController {
-    static async recibirDatosSensor(req,res) {
+    static async recibirDatosSensor(req, res) {
         try {
             const { zona, temperatura, humedad, peso_actual } = req.body;
 
             if (!zona || temperatura === undefined || humedad === undefined) {
-                return res.status(400).json({ error: "Faltan datos (zona, temperatura, humedad)" });
+                return res.status(400).json({ error: "Faltan datos" });
             }
 
-            console.log(`📥 API: Recibido de ${zona} -> T:${temperatura} H:${humedad} P:${peso_actual}`);
+            console.log(`📥 API: Recibido de ${zona} -> T:${temperatura} H:${humedad}`);
 
-            // if (req.io) {
-            //     req.io.emit('actualizacion_sensores', {
-            //         zona: zona,
-            //         temperatura: temperatura,
-            //         humedad: humedad,
-            //         peso: peso_actual,
-            //         timestamp: new Date()
-            //     });
-            //     console.log("📡 Datos enviados a la App Móvil vía Socket");
-            // } else {
-            //     console.warn("⚠️ No se encontró instancia de Socket.io (req.io)");
-            // }
-
-            // Llamamos al servicio (Caja negra)
-            const alertas = await prologService.procesarDatosSensor(zona, temperatura, humedad);
-
-            if (alertas.length > 0) {
-                console.log("   🔥 Respondiendo ALERTA al ESP32");
-                return res.status(200).json({ status: "ALERTA", data: alertas });
+            // 1. Llamada al servicio
+            const resultado = await prologService.procesarDatosSensor(zona, temperatura, humedad);
+            
+            if (resultado.alertas.length > 0) {
+                console.log(`   🚨 ESTADO CRÍTICO: ${resultado.estado}`);
+                
+                return res.status(200).json({ 
+                    status: resultado.estado,  // "AGOTADO" o "PELIGRO"
+                    data: resultado.alertas    // Lista de productos afectados
+                });
             }
 
+            // 3. Todo bien
             return res.status(200).json({ status: "OK", data: [] });
+
         } catch (error) {
             console.error("Error en controlador:", error);
-            return res.status(500).json({ error: "Error interno del servidor" });
+            return res.status(500).json({ error: "Error interno" });
         }
     }
 }
